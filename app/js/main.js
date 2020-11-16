65b8f8c5b033bcfe8cc1cdb08d38bd63bef6aa55
liss5555 <liss5555@rambler.ru>
2020-11-16 03:53:48 +0200

    added section slider-products

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,18 @@ $(function(){
 
 	var mixer = mixitup('.products__inner-box');
 
+	$(".rate-star").rateYo({    
+    rating: 5,
+		starWidth: "12px",
+		readOnly: true,
+	});
+	
+	$(".slider-products__inner").slick({
+		dots: true,
+		arrows: false,
+		slidesToShow: 4,
+		slidesToScroll: 4
+
+	});
+
 });
